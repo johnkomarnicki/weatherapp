1629ba21d086afeddb09ee1d6e0fcb5b5960ded0
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-02-01 12:31:25 -0500

    section #7

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="main">
+    <Modal v-if="modalOpen" v-on:close-modal="toggleModal" :APIkey="APIkey" />
+    <Navigation v-on:add-city="toggleModal" />
+    <router-view v-bind:cities="cities" />
   </div>
 </template>
 
+<script>
+import axios from "axios";
+import db from "./firebase/firebaseinit";
+import Navigation from "./components/Navigation";
+import Modal from "./components/Modal";
+export default {
+  name: "App",
+  components: {
+    Navigation,
+    Modal,
+  },
+  data() {
+    return {
+      APIkey: "339d698f17ac0be62378718a872f23c1",
+      cities: [],
+      modalOpen: null,
+    };
+  },
+  created() {
+    this.getCityWeather();
+  },
+  methods: {
+    getCityWeather() {
+      let firebaseDB = db.collection("cities");
+
+      firebaseDB.onSnapshot((snap) => {
+        snap.docChanges().forEach(async (doc) => {
+          console.log(doc.constructor());
+          console.log(doc.doc);
+          if (doc.type === "added") {
+            try {
+              const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${doc.doc.data().city}&units=imperial&APPID=${
+                  this.APIkey
+                }`
+              );
+              const data = response.data;
+              firebaseDB
+                .doc(doc.doc.id)
+                .update({
+                  currentWeather: data,
+                })
+                .then(() => {
+                  this.cities.push(doc.doc.data());
+                });
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        });
+      });
+    },
+    toggleModal() {
+      this.modalOpen = !this.modalOpen;
+    },
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
 }
 
-#nav {
-  padding: 30px;
+.main {
+  max-width: 1024px;
+  margin: 0 auto;
+  height: 100vh;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .container {
+    padding: 0 20px;
   }
 }
 </style>
