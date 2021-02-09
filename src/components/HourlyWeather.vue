29d1cde0289192ad66a9e3a8de35dac70dc73eca
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-02-08 23:08:34 -0500

    Section-13

---
+++ src/components/HourlyWeather.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hourly-weather">
+    <div class="container">
+      <div class="hourly-temp" v-for="(time, index) in filteredList" :key="index">
+        <HourlyTemp :time="time" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HourlyTemp from "./HourlyTemp";
+export default {
+  name: "HourlyWeather",
+  props: ["forecast"],
+  components: {
+    HourlyTemp,
+  },
+  created() {
+    console.log(this.forecast);
+  },
+  computed: {
+    filteredList() {
+      return this.forecast.hourly.slice(0, 23);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.hourly-weather {
+  padding: 30px 0;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.7);
+
+  .container {
+    display: flex;
+    max-width: 100vw;
+    overflow: scroll;
+    padding: 0;
+    margin: 0 20px;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+}
+</style>
--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -6,6 +6,7 @@
     <div v-else class="weather" :class="{ day: isDay, night: isNight }">
       <div class="weather-wrap">
         <CurrentWeather :isDay="isDay" :isNight="isNight" :currentWeather="currentWeather" />
+        <HourlyWeather :forecast="forecast" />
       </div>
     </div>
   </div>
@@ -15,11 +16,13 @@
 import axios from "axios";
 import db from "../firebase/firebaseinit";
 import CurrentWeather from "../components/CurrentWeather";
+import HourlyWeather from "../components/HourlyWeather";
 export default {
   name: "Weather",
   props: ["APIkey", "isDay", "isNight"],
   components: {
     CurrentWeather,
+    HourlyWeather,
   },
   data() {
     return {
